9e024f47052d8681c04cf6d16f758cb49e633287
AndyMcB <1andymc1@gmail.com>
2016-05-03 15:24:40 +0100

    Added files via upload

---
+++ weatherWidget.js
@@ -0,0 +1,4 @@
+<div id="cont_MzY0OTN8NXw0fDV8MXxBREM5NDJ8MXxGRkZGRkZ8Q3wxfGttcGg-">
+  <div id="spa_MzY0OTN8NXw0fDV8MXxBREM5NDJ8MXxGRkZGRkZ8Q3wxfGttcGg-"><a id="a_MzY0OTN8NXw0fDV8MXxBREM5NDJ8MXxGRkZGRkZ8Q3wxfGttcGg-" rel="nofollow" href="http://www.weather-wherever.co.uk/ireland/galway_v36493/" target="_blank" style="color:#333;text-decoration:none;">Galway Weather forecast</a></div>
+  <script type="text/javascript" src="http://widget.weather-wherever.co.uk/js/MzY0OTN8NXw0fDV8MXxBREM5NDJ8MXxGRkZGRkZ8Q3wxfGttcGg-"></script>
+</div>
